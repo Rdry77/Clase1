af541c9046b5bb5732e871370f2a2c755db1bc2d
Rudy <rdry210@gmail.com>
2025-10-16 11:40:13 -0500

    Segunda tarea arreglada

--- utils/functions.py
+++ utils/functions.py
@@ -31,4 +31,16 @@ def build_logistic_figure(P0: float, r: float, K: float, t_max: float, npoints: 
         yaxis_title="P(t)",
         margin=dict(l=40, r=20, t=40, b=40)
     )
+
+    fig.update_xaxes(
+    showgrid=True, gridwidth=1, gridcolor='lightpink',
+    zeroline=True, zerolinewidth=2, zerolinecolor='red',
+    showline=True, linecolor='black', linewidth=2, mirror=True,
+    )
+
+    fig.update_yaxes(
+        showgrid=True, gridwidth=1, gridcolor='lightpink',
+        zeroline=True, zerolinewidth=2, zerolinecolor='red',
+        showline=True, linecolor='black', linewidth=2, mirror=True,
+    )
     return fig
